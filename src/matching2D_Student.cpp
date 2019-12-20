c9ff380119950525a965951cd89eee2f974dfd81
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-12-20 01:03:26 -0800

    implement harris keypoint detector implement detKeypointsModern that chooses one of the detectors based on the passed detectorType variable

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -61,6 +61,50 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
     cout << descriptorType << " descriptor extraction in " << 1000 * t / 1.0 << " ms" << endl;
 }
 
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+{
+    if(detectorType.compare("SHITOMASI") == 0)
+    {
+        detKeypointsShiTomasi(keypoints, img, bVis);
+    }
+    else if (detectorType.compare("HARRIS") == 0)
+    {
+        detKeypointsHarris(keypoints, img, bVis);
+    }
+    else if (detectorType.compare("FAST") == 0)
+    {
+        auto detector = cv::FastFeatureDetector::create(34, true);
+        detector->detect(img, keypoints);
+    }
+    else if (detectorType.compare("BRISK") == 0)
+    {
+        auto detector = cv::BRISK::create(34);
+        detector->detect(img, keypoints);
+    }
+    else if (detectorType.compare("ORB") == 0)
+    {
+        auto detector = cv::ORB::create();
+        detector->detect(img, keypoints);
+    }
+    else if (detectorType.compare("AKAZE") == 0)
+    {
+        auto akazeDetector = cv::AKAZE::create();
+        akazeDetector->detect(img, keypoints);
+        //detector->detect(img, keypoints);
+    }
+    else if (detectorType.compare("SIFT") == 0)
+    {
+        auto siftDetector = cv::xfeatures2d::SIFT::create();
+        siftDetector->detect(img, keypoints);
+        //detector->detect(img, keypoints);
+    }
+    else
+    {
+        // error the descriptor has to be one of the above descriptors
+        exit(1);
+    }
+}
+
 // Detect keypoints in image using the traditional Shi-Thomasi detector
 void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
 {
@@ -99,5 +143,81 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         cv::namedWindow(windowName, 6);
         imshow(windowName, visImage);
         cv::waitKey(0);
+    }
+}
+
+void detKeypointsHarris(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+{
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // visualize results
+    if(bVis)
+    {
+        string windowName = "Harris Corner Detector Response Matrix";
+        cv::namedWindow(windowName, 4);
+        cv::imshow(windowName, dst_norm_scaled);
+        cv::waitKey(0);
+    }
+
+    // STUDENTS NEET TO ENTER THIS CODE (C3.2 Atom 4)
+
+    // Look for prominent corners and instantiate keypoints
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+    for (size_t j = 0; j < dst_norm.rows; j++)
+    {
+        for (size_t i = 0; i < dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if (response > minResponse)
+            { // only store points above a threshold
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     // eof loop over rows
+
+    // visualize keypoints
+    if(bVis)
+    {
+        string windowName = "Harris Corner Detection Results";
+        cv::namedWindow(windowName, 5);
+        cv::Mat visImage = dst_norm_scaled.clone();
+        cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::imshow(windowName, visImage);
+        cv::waitKey(0);
+        // EOF STUDENT CODE
     }
 }
