e6f9a2aebc06b6feefc3ab86d5f45eda3d0cbd2e
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-12-20 05:06:03 -0800

    Implement task4 which is descriptor extractor like brief, orb, freak, akaze, and sift

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -48,11 +48,33 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 
         extractor = cv::BRISK::create(threshold, octaves, patternScale);
     }
+    else if(descriptorType.compare("BRIEF") == 0)
+    {
+        extractor = cv::xfeatures2d::BriefDescriptorExtractor::create();
+    }
+    else if(descriptorType.compare("ORB") == 0)
+    {
+        extractor = cv::ORB::create();
+    }
+    else if(descriptorType.compare("FREAK") == 0)
+    {
+        extractor = cv::xfeatures2d::FREAK::create();
+    }
+    else if(descriptorType.compare("AKAZE") == 0)
+    {
+        extractor = cv::AKAZE::create();
+    }
+    else if(descriptorType.compare("SIFT") == 0)
+    {
+        extractor = cv::xfeatures2d::SiftDescriptorExtractor::create();
+    }
     else
     {
-
-        //...
+        //Error
+        cout<< "Error no valid descriptor extractor is chosen. Program exit\n";
+        exit(1);
     }
+
 
     // perform feature description
     double t = (double)cv::getTickCount();
